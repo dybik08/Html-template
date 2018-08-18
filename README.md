cc3f39e26202844804fa44adfa6eb9c976d55300
dybik08 <dybik08@gmail.com>
2018-08-18 20:25:31 +0200

    Template prepared and working

---
+++ README.md
@@ -0,0 +1,7 @@
+# Html template with webpack configured and ready to use. I'm not using webpack wersion older than ~4 due to setup problems I had.
+
+* clone repository
+* npm init
+* npm run build
+
+## Enjoy!
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+let path = require('path');
+let webpack = require('webpack');
+
+module.exports = {
+    entry: './src/js/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+    ]
+};
